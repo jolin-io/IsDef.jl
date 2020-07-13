2b4f5208caf64c264dd471533dcac4368689e560
Stephan Sahm <stephan.sahm@gmx.de>
2020-07-13 21:16:42 +0200

    adding some tests to improve coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,63 +3,74 @@ using Test
 using Documenter
 import FunctionWrappers: FunctionWrapper
 
-# test apply
-# ----------
+function test_everything()
+    # test apply
+    # ----------
 
-@test apply(reduce, +, [2,3], init=1) == reduce(+, [2,3], init=1)
-
-
-# test isdef/Out
-# --------------
-
-@test Out(Base.map, typeof(x->2x), Vector{Int}) == Vector{Int}
-@test isdef(Base.reduce, typeof(+), Vector{<:Number})
-
-# Caution!! Core.Compiler.return_type is actually not always as good as expected:
-# this by now actually inferes correctly...
-# Base.promote_op(Base.reduce, typeof(+), Vector{String}) === Union{}
+    @test apply(reduce, +, [2,3], init=1) == reduce(+, [2,3], init=1)
 
 
-# works transparent with wrappers (unlike Base.`which`)
-wrapper(args...; kwargs...) = original(args...; kwargs...)
-original(a::Int, b::String) = true
-@test isdef(wrapper, Int, String)
-@test !isdef(wrapper, Float64)
+    # test isdef/Out
+    # --------------
+
+    @test Out(Base.map, typeof(x->2x), Vector{Int}) == Vector{Int}
+    @test isdef(Base.reduce, typeof(+), Vector{<:Number})
+
+    # Caution!! Core.Compiler.return_type is actually not always as good as expected:
+    # this by now actually inferes correctly...
+    # Base.promote_op(Base.reduce, typeof(+), Vector{String}) === Union{}
 
 
-# does even work on compiler level
-@test Out(Out, typeof(Base.map), typeof(x->2x), Vector{Int}) == Type{Vector{Int}}
+    # works transparent with wrappers (unlike Base.`which`)
+    wrapper(args...; kwargs...) = original(args...; kwargs...)
+    original(a::Int, b::String) = true
+    @test isdef(wrapper, Int, String)
+    @test !isdef(wrapper, Float64)
 
 
-# works in a strict open sense only with Any (would have to work for a whole newtype)
-# for everything else the concrete leave-types are used
-f(a) = a + a
-# we decided to leave Any as is, not going to newtype, and hence Any says often yes instead of no
-@test isdef(f, Any)
-@test isdef(f, Number)
-@test isdef(f, Integer)
-@test !isdef(f, String)
-@test !isdef(f, AbstractString)
-@test !isdef(f, Vector{<:String})
-@test !isdef(f, Vector{<:AbstractString})
-
-@test isdef(Base.map, typeof(x -> x+4), Array{<:Number, 3})
-@test !isdef(Base.map, typeof(x -> x+4), Vector{String})
-@test !isdef(Base.map, typeof(x -> x+4), Vector{AbstractString})
-@test !isdef(Base.map, typeof(x -> x+4), Vector{<:AbstractString})
+    # does even work on compiler level
+    @test Out(Out, typeof(Base.map), typeof(x->2x), Vector{Int}) == Type{Vector{Int}}
 
 
-@test Out(Some, Int) == Some{Int}
-@test isdef(Some, Int)
+    # works in a strict open sense only with Any (would have to work for a whole newtype)
+    # for everything else the concrete leave-types are used
+    f(a) = a + a
+    # we decided to leave Any as is, not going to newtype, and hence Any says often yes instead of no
+    @test isdef(f, Any)
+    @test isdef(f, Number)
+    @test isdef(f, Integer)
+    @test !isdef(f, String)
+    @test !isdef(f, AbstractString)
+    @test !isdef(f, Vector{<:String})
+    @test !isdef(f, Vector{<:AbstractString})
+
+    @test isdef(Base.map, typeof(x -> x+4), Array{<:Number, 3})
+    @test !isdef(Base.map, typeof(x -> x+4), Vector{String})
+    @test !isdef(Base.map, typeof(x -> x+4), Vector{AbstractString})
+    @test !isdef(Base.map, typeof(x -> x+4), Vector{<:AbstractString})
 
 
-# test documentation
-# ------------------
+    # test types
+    @test Out(Some, Int) == Some{Int}
+    @test isdef(Some, Int)
+
+    # test values
+    @test Out(sin, 1) == Float64
+    @test isdef(sin, 1) 
+
+
+    # test documentation
+    # ------------------
+
+    doctest(IsDef)
+    @test Out(Base.map, typeof(string), Vector{Int}) == Vector{String}
+    @test Out(Base.map, typeof(isodd), Vector{Int}) == Vector{Bool}
+    @test Out(Base.map, FunctionWrapper{Bool, Tuple{Any}}, Vector{Int}) == Vector{Bool}
+
+end
+
 
 # check that if we redefine the generated functions of the module, nothing breaks
+# ----------------------------
 IsDef.@redefine_generated
-
-doctest(IsDef)
-@test Out(Base.map, typeof(string), Vector{Int}) == Vector{String}
-@test Out(Base.map, typeof(isodd), Vector{Int}) == Vector{Bool}
-@test Out(Base.map, FunctionWrapper{Bool, Tuple{Any}}, Vector{Int}) == Vector{Bool}
+test_everything()
